92f1bca610a07f33d34ef2437edba7289b9ae142
MisaghMomeniB <jenabemisagh@gmail.com>
2024-10-24 22:30:19 +0330

    Calculator Class

--- source_code.py
+++ source_code.py
@@ -1,2 +1,12 @@
 from tkinter import Tk, Entry, Button, StringVar
 
+class Calculator:
+    def __init__(self, master):
+        master.title("Calculator")
+        master.geometry('357x420+0+0')
+        master.config(bg='purple')
+        master.resizable(False, False)
+
+        self.equation = StringVar()
+        self.entry_value = ''
+
